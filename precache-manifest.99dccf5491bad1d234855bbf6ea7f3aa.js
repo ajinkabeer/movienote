a8121c5a7575bf65cfeaeecbcd2091693c0bfe0f
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-04-20 14:55:37 +0530

    Updates

---
+++ precache-manifest.99dccf5491bad1d234855bbf6ea7f3aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592cbf4b4968d02400b049bb75177312",
+    "url": "/movienote/index.html"
+  },
+  {
+    "revision": "a592f9aeb850287c5476",
+    "url": "/movienote/static/css/main.86398bd5.chunk.css"
+  },
+  {
+    "revision": "f17a1da8b992d79951be",
+    "url": "/movienote/static/js/2.f43bb734.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movienote/static/js/2.f43bb734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a592f9aeb850287c5476",
+    "url": "/movienote/static/js/main.af92a27a.chunk.js"
+  },
+  {
+    "revision": "261f0630b045841d7f7a",
+    "url": "/movienote/static/js/runtime-main.0a6f74ec.js"
+  }
+]);
