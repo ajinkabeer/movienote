4a0d1a3989283db06b4dd15655be7c7d4006a3ed
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-04-20 12:45:03 +0530

    Add hooks in search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 export function Search() {
-  return <input type="text" placeholder="search for a movie" />;
+  const [input, setInput] = useState("");
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  return (
+    <>
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="search for a movie"
+        value={input}
+        onChange={handleChange}
+      />
+    </>
+  );
 }
