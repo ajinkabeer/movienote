61c37df9c7c3a80e5a194b02ad2f77adb7047e96
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-04-20 12:17:02 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./css/app.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App({ movies }) {
+  const movieList = movies.map((movie) => {
+    return (
+      <li className={movie.meter} key={movie.id}>
+        {movie.movie}
+      </li>
+    );
+  });
+
+  return <div className="main">{<ul>{movieList}</ul>}</div>;
 }
 
 export default App;
---
+++ src/data/index.js
@@ -0,0 +1,1007 @@
+export const movies = [
+  {
+    id: 0,
+    movie: "The 40-Year-Old Virgin",
+    meter: "Fresh",
+  },
+  {
+    id: 1,
+    movie: "51 Birch Street",
+    meter: "Rotten",
+  },
+  {
+    id: 2,
+    movie: "The Adventures of Sharkboy and Lavagirl in 3-D",
+    meter: "Fresh",
+  },
+  {
+    id: 3,
+    movie: "on Flux",
+    meter: "Rotten",
+  },
+  {
+    id: 4,
+    movie: "Aliens of the Deep",
+    meter: "Fresh",
+  },
+  {
+    id: 5,
+    movie: "Alone in the Dark",
+    meter: "Rotten",
+  },
+  {
+    id: 6,
+    movie: "The Amityville Horror",
+    meter: "Fresh",
+  },
+  {
+    id: 7,
+    movie: "Are We There Yet?",
+    meter: "Rotten",
+  },
+  {
+    id: 8,
+    movie: "The Aristocrats",
+    meter: "Fresh",
+  },
+  {
+    id: 9,
+    movie: "Assault on Precinct 13",
+    meter: "Rotten",
+  },
+  {
+    id: 10,
+    movie: "Bad News Bears",
+    meter: "Fresh",
+  },
+  {
+    id: 11,
+    movie: "The Ballad of Jack and Rose",
+    meter: "Rotten",
+  },
+  {
+    id: 12,
+    movie: "Batman Begins",
+    meter: "Fresh",
+  },
+  {
+    id: 13,
+    movie: "The Baxter",
+    meter: "Rotten",
+  },
+  {
+    id: 14,
+    movie: "Be Cool",
+    meter: "Fresh",
+  },
+  {
+    id: 15,
+    movie: "Beauty Shop",
+    meter: "Rotten",
+  },
+  {
+    id: 16,
+    movie: "Because of Winn-Dixie",
+    meter: "Fresh",
+  },
+  {
+    id: 17,
+    movie: "Bee Season",
+    meter: "Rotten",
+  },
+  {
+    id: 18,
+    movie: "Bewitched",
+    meter: "Fresh",
+  },
+  {
+    id: 19,
+    movie: "Bigger Than the Sky",
+    meter: "Rotten",
+  },
+  {
+    id: 20,
+    movie: "Boogeyman",
+    meter: "Fresh",
+  },
+  {
+    id: 21,
+    movie: "Brick",
+    meter: "Rotten",
+  },
+  {
+    id: 22,
+    movie: "Brokeback Mountain",
+    meter: "Fresh",
+  },
+  {
+    id: 23,
+    movie: "Broken Flowers",
+    meter: "Rotten",
+  },
+  {
+    id: 24,
+    movie: "The Brothers Grimm",
+    meter: "Fresh",
+  },
+  {
+    id: 25,
+    movie: "Capote",
+    meter: "Rotten",
+  },
+  {
+    id: 26,
+    movie: "Casanova",
+    meter: "Fresh",
+  },
+  {
+    id: 27,
+    movie: "The Cave",
+    meter: "Rotten",
+  },
+  {
+    id: 28,
+    movie: "Charlie and the Chocolate Factory",
+    meter: "Fresh",
+  },
+  {
+    id: 29,
+    movie: "Cheaper by the Dozen 2",
+    meter: "Rotten",
+  },
+  {
+    id: 30,
+    movie: "Chicken Little",
+    meter: "Fresh",
+  },
+  {
+    id: 31,
+    movie: "The Chronicles of Narnia: The Lion, the Witch and the Wardrobe",
+    meter: "Rotten",
+  },
+  {
+    id: 32,
+    movie: "The Chumscrubber",
+    meter: "Fresh",
+  },
+  {
+    id: 33,
+    movie: "Cinderella Man",
+    meter: "Rotten",
+  },
+  {
+    id: 34,
+    movie: "Coach Carter",
+    meter: "Fresh",
+  },
+  {
+    id: 35,
+    movie: "Constantine",
+    meter: "Rotten",
+  },
+  {
+    id: 36,
+    movie: "Corpse Bride",
+    meter: "Fresh",
+  },
+  {
+    id: 37,
+    movie: "The Crow: Wicked Prayer",
+    meter: "Rotten",
+  },
+  {
+    id: 38,
+    movie: "Cruel World",
+    meter: "Fresh",
+  },
+  {
+    id: 39,
+    movie: "Cry_Wolf",
+    meter: "Rotten",
+  },
+  {
+    id: 40,
+    movie: "Cursed",
+    meter: "Fresh",
+  },
+  {
+    id: 41,
+    movie: "Daltry Calhoun",
+    meter: "Rotten",
+  },
+  {
+    id: 42,
+    movie: "Dark Water",
+    meter: "Fresh",
+  },
+  {
+    id: 43,
+    movie: "Deuce Bigalow: European Gigolo",
+    meter: "Rotten",
+  },
+  {
+    id: 44,
+    movie: "Derailed",
+    meter: "Fresh",
+  },
+  {
+    id: 45,
+    movie: "The Derby Stallion",
+    meter: "Rotten",
+  },
+  {
+    id: 46,
+    movie: "The Devil's Rejects",
+    meter: "Fresh",
+  },
+  {
+    id: 47,
+    movie: "Diary of a Mad Black Woman",
+    meter: "Rotten",
+  },
+  {
+    id: 48,
+    movie: "Dirty Love",
+    meter: "Fresh",
+  },
+  {
+    id: 49,
+    movie: "Dominion: Prequel to the Exorcist",
+    meter: "Rotten",
+  },
+  {
+    id: 50,
+    movie: "Domino",
+    meter: "Fresh",
+  },
+  {
+    id: 51,
+    movie: "Don't Come Knocking",
+    meter: "Rotten",
+  },
+  {
+    id: 52,
+    movie: "Doom",
+    meter: "Fresh",
+  },
+  {
+    id: 53,
+    movie: "Dreamer",
+    meter: "Rotten",
+  },
+  {
+    id: 54,
+    movie: "The Dukes of Hazzard",
+    meter: "Fresh",
+  },
+  {
+    id: 55,
+    movie: "Duma",
+    meter: "Rotten",
+  },
+  {
+    id: 56,
+    movie: "The Dying Gaul",
+    meter: "Fresh",
+  },
+  {
+    id: 57,
+    movie: "Elektra",
+    meter: "Rotten",
+  },
+  {
+    id: 58,
+    movie: "Elizabethtown",
+    meter: "Fresh",
+  },
+  {
+    id: 59,
+    movie: "Enron: The Smartest Guys in the Room",
+    meter: "Rotten",
+  },
+  {
+    id: 60,
+    movie: "Everything Is Illuminated",
+    meter: "Fresh",
+  },
+  {
+    id: 61,
+    movie: "The Exorcism of Emily Rose",
+    meter: "Rotten",
+  },
+  {
+    id: 62,
+    movie: "The Family Stone",
+    meter: "Fresh",
+  },
+  {
+    id: 63,
+    movie: "Fantastic Four",
+    meter: "Rotten",
+  },
+  {
+    id: 64,
+    movie: "Favela Rising",
+    meter: "Fresh",
+  },
+  {
+    id: 65,
+    movie: "Fever Pitch",
+    meter: "Rotten",
+  },
+  {
+    id: 66,
+    movie: "First Descent",
+    meter: "Fresh",
+  },
+  {
+    id: 67,
+    movie: "Flightplan",
+    meter: "Rotten",
+  },
+  {
+    id: 68,
+    movie: "The Fog",
+    meter: "Fresh",
+  },
+  {
+    id: 69,
+    movie: "Forty Shades of Blue",
+    meter: "Rotten",
+  },
+  {
+    id: 70,
+    movie: "Four Brothers",
+    meter: "Fresh",
+  },
+  {
+    id: 71,
+    movie: "Frankenstein vs. the Creature from Blood Cove",
+    meter: "Rotten",
+  },
+  {
+    id: 72,
+    movie: "Fun with Dick and Jane",
+    meter: "Fresh",
+  },
+  {
+    id: 73,
+    movie: "The Game of Their Lives",
+    meter: "Rotten",
+  },
+  {
+    id: 74,
+    movie: "Get Rich or Die Tryin'",
+    meter: "Fresh",
+  },
+  {
+    id: 75,
+    movie: "Getting Played",
+    meter: "Rotten",
+  },
+  {
+    id: 76,
+    movie: "Goal!",
+    meter: "Fresh",
+  },
+  {
+    id: 77,
+    movie: "The Gospel",
+    meter: "Rotten",
+  },
+  {
+    id: 78,
+    movie: "Good Night, and Good Luck",
+    meter: "Fresh",
+  },
+  {
+    id: 79,
+    movie: "The Great Raid",
+    meter: "Rotten",
+  },
+  {
+    id: 80,
+    movie: "The Greatest Game Ever Played",
+    meter: "Fresh",
+  },
+  {
+    id: 81,
+    movie: "Green Street",
+    meter: "Rotten",
+  },
+  {
+    id: 82,
+    movie: "Grizzly Man",
+    meter: "Fresh",
+  },
+  {
+    id: 83,
+    movie: "Guess Who",
+    meter: "Rotten",
+  },
+  {
+    id: 84,
+    movie: "Happy Endings",
+    meter: "Fresh",
+  },
+  {
+    id: 85,
+    movie: "Harry Potter and the Goblet of Fire",
+    meter: "Rotten",
+  },
+  {
+    id: 86,
+    movie: "Her Minor Thing",
+    meter: "Fresh",
+  },
+  {
+    id: 87,
+    movie: "Herbie: Fully Loaded",
+    meter: "Rotten",
+  },
+  {
+    id: 88,
+    movie: "Hide and Seek",
+    meter: "Fresh",
+  },
+  {
+    id: 89,
+    movie: "A History of Violence",
+    meter: "Rotten",
+  },
+  {
+    id: 90,
+    movie: "Hitch",
+    meter: "Fresh",
+  },
+  {
+    id: 91,
+    movie: "The Hitchhiker's Guide to the Galaxy",
+    meter: "Rotten",
+  },
+  {
+    id: 92,
+    movie: "The Honeymooners",
+    meter: "Fresh",
+  },
+  {
+    id: 93,
+    movie: "Hoodwinked!",
+    meter: "Rotten",
+  },
+  {
+    id: 94,
+    movie: "Hostage",
+    meter: "Fresh",
+  },
+  {
+    id: 95,
+    movie: "Hostel",
+    meter: "Rotten",
+  },
+  {
+    id: 96,
+    movie: "House of D",
+    meter: "Fresh",
+  },
+  {
+    id: 97,
+    movie: "House of Wax",
+    meter: "Rotten",
+  },
+  {
+    id: 98,
+    movie: "Hustle & Flow",
+    meter: "Fresh",
+  },
+  {
+    id: 99,
+    movie: "The Ice Harvest",
+    meter: "Rotten",
+  },
+  {
+    id: 100,
+    movie: "Ice Princess",
+    meter: "Fresh",
+  },
+  {
+    id: 101,
+    movie: "In Her Shoes",
+    meter: "Rotten",
+  },
+  {
+    id: 102,
+    movie: "In the Mix",
+    meter: "Fresh",
+  },
+  {
+    id: 103,
+    movie: "Inside Deep Throat",
+    meter: "Rotten",
+  },
+  {
+    id: 104,
+    movie: "Intermedio",
+    meter: "Fresh",
+  },
+  {
+    id: 105,
+    movie: "The Interpreter",
+    meter: "Rotten",
+  },
+  {
+    id: 106,
+    movie: "Into the Blue",
+    meter: "Fresh",
+  },
+  {
+    id: 107,
+    movie: "The Island",
+    meter: "Rotten",
+  },
+  {
+    id: 108,
+    movie: "The Jacket",
+    meter: "Fresh",
+  },
+  {
+    id: 109,
+    movie: "Jarhead",
+    meter: "Rotten",
+  },
+  {
+    id: 110,
+    movie: "Junebug",
+    meter: "Fresh",
+  },
+  {
+    id: 111,
+    movie: "Just Friends",
+    meter: "Rotten",
+  },
+  {
+    id: 112,
+    movie: "Just Like Heaven",
+    meter: "Fresh",
+  },
+  {
+    id: 113,
+    movie: "Kicking & Screaming",
+    meter: "Rotten",
+  },
+  {
+    id: 114,
+    movie: "Kids in America",
+    meter: "Fresh",
+  },
+  {
+    id: 115,
+    movie: "King Kong",
+    meter: "Rotten",
+  },
+  {
+    id: 116,
+    movie: "King's Ransom",
+    meter: "Fresh",
+  },
+  {
+    id: 117,
+    movie: "Kingdom of Heaven",
+    meter: "Rotten",
+  },
+  {
+    id: 118,
+    movie: "Kiss Kiss Bang Bang",
+    meter: "Fresh",
+  },
+  {
+    id: 119,
+    movie: "The L.A. Riot Spectacular",
+    meter: "Rotten",
+  },
+  {
+    id: 120,
+    movie: "Land of the Dead",
+    meter: "Fresh",
+  },
+  {
+    id: 121,
+    movie: "Lassie",
+    meter: "Rotten",
+  },
+  {
+    id: 122,
+    movie: "Last Days",
+    meter: "Fresh",
+  },
+  {
+    id: 123,
+    movie: "The League of Gentlemen's Apocalypse",
+    meter: "Rotten",
+  },
+  {
+    id: 124,
+    movie: "The Legend of Zorro",
+    meter: "Fresh",
+  },
+  {
+    id: 125,
+    movie: "Long Distance",
+    meter: "Rotten",
+  },
+  {
+    id: 126,
+    movie: "The Longest Yard",
+    meter: "Fresh",
+  },
+  {
+    id: 127,
+    movie: "Lord of War",
+    meter: "Rotten",
+  },
+  {
+    id: 128,
+    movie: "Lords of Dogtown",
+    meter: "Fresh",
+  },
+  {
+    id: 129,
+    movie: "The Lost City",
+    meter: "Rotten",
+  },
+  {
+    id: 130,
+    movie: "A Lot Like Love",
+    meter: "Fresh",
+  },
+  {
+    id: 131,
+    movie: "Mad Hot Ballroom",
+    meter: "Rotten",
+  },
+  {
+    id: 132,
+    movie: "Madagascar",
+    meter: "Fresh",
+  },
+  {
+    id: 133,
+    movie: "The Man",
+    meter: "Rotten",
+  },
+  {
+    id: 134,
+    movie: "Man of the House",
+    meter: "Fresh",
+  },
+  {
+    id: 135,
+    movie: "Marilyn Hotchkiss' Ballroom Dancing and Charm School",
+    meter: "Rotten",
+  },
+  {
+    id: 136,
+    movie: "The Matador",
+    meter: "Fresh",
+  },
+  {
+    id: 137,
+    movie: "Match Point",
+    meter: "Rotten",
+  },
+  {
+    id: 138,
+    movie: "Me and You and Everyone We Know",
+    meter: "Fresh",
+  },
+  {
+    id: 139,
+    movie: "Memoirs of a Geisha",
+    meter: "Rotten",
+  },
+  {
+    id: 140,
+    movie: "MirrorMask",
+    meter: "Fresh",
+  },
+  {
+    id: 141,
+    movie: "Miss Congeniality 2: Armed and Fabulous",
+    meter: "Rotten",
+  },
+  {
+    id: 142,
+    movie: "Monster-in-Law",
+    meter: "Fresh",
+  },
+  {
+    id: 143,
+    movie: "The Mostly Unfabulous Social Life of Ethan Green",
+    meter: "Rotten",
+  },
+  {
+    id: 144,
+    movie: "Mr. & Mrs. Smith",
+    meter: "Fresh",
+  },
+  {
+    id: 145,
+    movie: "Mrs. Palfrey at the Claremont",
+    meter: "Rotten",
+  },
+  {
+    id: 146,
+    movie: "Munich",
+    meter: "Fresh",
+  },
+  {
+    id: 147,
+    movie: "Murderball",
+    meter: "Rotten",
+  },
+  {
+    id: 148,
+    movie: "Must Love Dogs",
+    meter: "Fresh",
+  },
+  {
+    id: 149,
+    movie: "Nanny McPhee",
+    meter: "Rotten",
+  },
+  {
+    id: 150,
+    movie: "Never Been Thawed",
+    meter: "Fresh",
+  },
+  {
+    id: 151,
+    movie: "The New World",
+    meter: "Rotten",
+  },
+  {
+    id: 152,
+    movie: "Neverwas",
+    meter: "Fresh",
+  },
+  {
+    id: 153,
+    movie: "New York Doll",
+    meter: "Rotten",
+  },
+  {
+    id: 154,
+    movie: "Nine Lives",
+    meter: "Fresh",
+  },
+  {
+    id: 155,
+    movie: "No Direction Home",
+    meter: "Rotten",
+  },
+  {
+    id: 156,
+    movie: "North Country",
+    meter: "Fresh",
+  },
+  {
+    id: 157,
+    movie: "Our Very Own",
+    meter: "Rotten",
+  },
+  {
+    id: 158,
+    movie: "The Pacifier",
+    meter: "Fresh",
+  },
+  {
+    id: 159,
+    movie: "The Perfect Man",
+    meter: "Rotten",
+  },
+  {
+    id: 160,
+    movie: "Pooh's Heffalump Movie",
+    meter: "Fresh",
+  },
+  {
+    id: 161,
+    movie: "Popstar",
+    meter: "Rotten",
+  },
+  {
+    id: 162,
+    movie: "Pretty Persuasion",
+    meter: "Fresh",
+  },
+  {
+    id: 163,
+    movie: "Prime",
+    meter: "Rotten",
+  },
+  {
+    id: 164,
+    movie: "The Prize Winner of Defiance, Ohio",
+    meter: "Fresh",
+  },
+  {
+    id: 165,
+    movie: "The Producers",
+    meter: "Rotten",
+  },
+  {
+    id: 166,
+    movie: "Proof",
+    meter: "Fresh",
+  },
+  {
+    id: 167,
+    movie: "Racing Stripes",
+    meter: "Rotten",
+  },
+  {
+    id: 168,
+    movie: "Rebound",
+    meter: "Fresh",
+  },
+  {
+    id: 169,
+    movie: "Red Eye",
+    meter: "Rotten",
+  },
+  {
+    id: 170,
+    movie: "Rent",
+    meter: "Fresh",
+  },
+  {
+    id: 171,
+    movie: "The Ring Two",
+    meter: "Rotten",
+  },
+  {
+    id: 172,
+    movie: "The Ringer",
+    meter: "Fresh",
+  },
+  {
+    id: 173,
+    movie: "Rize",
+    meter: "Rotten",
+  },
+  {
+    id: 174,
+    movie: "Robots",
+    meter: "Fresh",
+  },
+  {
+    id: 175,
+    movie: "Roll Bounce",
+    meter: "Rotten",
+  },
+  {
+    id: 176,
+    movie: "Rumor Has It...",
+    meter: "Fresh",
+  },
+  {
+    id: 177,
+    movie: "Sahara",
+    meter: "Rotten",
+  },
+  {
+    id: 178,
+    movie: "Sarah Silverman: Jesus Is Magic",
+    meter: "Fresh",
+  },
+  {
+    id: 179,
+    movie: "Saw II",
+    meter: "Rotten",
+  },
+  {
+    id: 180,
+    movie: "Serenity",
+    meter: "Fresh",
+  },
+  {
+    id: 181,
+    movie: "Shadowboxer",
+    meter: "Rotten",
+  },
+  {
+    id: 182,
+    movie: "Shopgirl",
+    meter: "Fresh",
+  },
+  {
+    id: 183,
+    movie: "Sin City",
+    meter: "Rotten",
+  },
+  {
+    id: 184,
+    movie: "The Sisterhood of the Traveling Pants",
+    meter: "Fresh",
+  },
+  {
+    id: 185,
+    movie: "The Sisters",
+    meter: "Rotten",
+  },
+  {
+    id: 186,
+    movie: "The Skeleton Key",
+    meter: "Fresh",
+  },
+  {
+    id: 187,
+    movie: "Sky High",
+    meter: "Rotten",
+  },
+  {
+    id: 188,
+    movie: "Smile",
+    meter: "Fresh",
+  },
+  {
+    id: 189,
+    movie: "Son of the Mask",
+    meter: "Rotten",
+  },
+  {
+    id: 190,
+    movie: "A Sound of Thunder",
+    meter: "Fresh",
+  },
+  {
+    id: 191,
+    movie: "The Squid and the Whale",
+    meter: "Rotten",
+  },
+  {
+    id: 192,
+    movie: "Star Wars Episode III: Revenge of the Sith",
+    meter: "Fresh",
+  },
+  {
+    id: 193,
+    movie: "Stay",
+    meter: "Rotten",
+  },
+  {
+    id: 194,
+    movie: "Stealth",
+    meter: "Fresh",
+  },
+  {
+    id: 195,
+    movie: "Street Fight",
+    meter: "Rotten",
+  },
+  {
+    id: 196,
+    movie: "Supercross",
+    meter: "Fresh",
+  },
+  {
+    id: 197,
+    movie: "Syriana",
+    meter: "Rotten",
+  },
+  {
+    id: 198,
+    movie: "The Thing About My Folks",
+    meter: "Fresh",
+  },
+  {
+    id: 199,
+    movie: "The Three Burials of Melquiades Estrada",
+    meter: "Rotten",
+  },
+  {
+    id: 200,
+    movie: "Thru the Moebius Strip",
+    meter: "Fresh",
+  },
+];
