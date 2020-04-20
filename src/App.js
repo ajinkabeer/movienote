26effad6378bb86fc0eb0f08e090603c25e89da8
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-04-20 12:39:06 +0530

    Add search component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { MoviePicker } from "./components/MoviePicker";
+import { Search } from "./components/Search";
+
 import "./css/app.css";
 
 function App({ movies }) {
   return (
     <div className="main">
       <h1>Movie Note</h1>
+      <Search />
       <MoviePicker movies={movies} />
     </div>
   );
